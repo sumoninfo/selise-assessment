3112bb9bd73882e761d112b9fc9b3880d591f50d
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2023-01-21 14:27:36 +0600

    Update TopBar.vue

--- frotnend/src/views/admin/layouts/TopBar.vue
+++ frotnend/src/views/admin/layouts/TopBar.vue
@@ -1,37 +1,40 @@
 <template>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <router-link :to="{name:'HomePage'}" class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-      GeekPicker Assignment
+      {{ siteName }}
     </router-link>
     <div class="navbar-nav">
       <div class="nav-item text-nowrap">
-        <a @click="logout" class="nav-link px-3" href="#"><i class="fas fa-sign-out-alt"></i> Sign out</a>
+        <a @click="logoutSubmit" class="nav-link px-3" href="#"><i class="fas fa-sign-out-alt"></i> Sign out</a>
       </div>
     </div>
   </header>
 </template>
 
-<script>
-
-import ApiService          from "@/services/api.service";
+<script setup>
 import NotificationService from "@/services/notification.service";
 import JwtService          from "@/services/jwt.service";
+import handleAuth          from "@/services/modules/auth";
+import {useRouter}         from "vue-router";
+import {useStore}          from "vuex";
 
-export default {
-  methods: {
-    logout() {
-      const token = JwtService.getToken();
-      if (typeof token != "undefined") {
-        ApiService.post('/logout').then(response => {
-          JwtService.destroyToken();
-          this.$store.commit('auth/GETUSER', {})
-          this.$router.push({name: "LoginPage"});
-          NotificationService.success('Logout successful');
-        }).catch(error => {
-          NotificationService.error(error.response.data.message);
-        })
-      }
-    },
+const {logout} = handleAuth();
+import {ref}               from "vue";
+
+const siteName     = ref(process.env.VUE_APP_TITLE)
+const logoutSubmit = () => {
+  const token  = JwtService.getToken();
+  const router = useRouter()
+  const store  = useStore()
+  if (typeof token != "undefined") {
+    logout().then(({data}) => {
+      JwtService.destroyToken();
+      store.commit('auth/GETUSER', {})
+      router.push({name: "LoginPage"})
+      NotificationService.success('Logout successful');
+    }).catch(error => {
+      NotificationService.error(error.response.data.message);
+    })
   }
 }
 </script>
